97cd787255e32f4d44372b98c0c10a2dacb25591
Nikhil Acharya <acharyanikhil2004@gmail.com>
2024-09-14 02:07:44 -0400

    Added convex hooey

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-blue-400">conartAI</h1> 
+      <h1 className="text-blue-400">conartAI</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
