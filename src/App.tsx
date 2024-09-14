5ab42564317e72fad7c2ae0631fc7689375e00a3
benjamindrussell <ben@benjamindrussell.com>
2024-09-14 15:03:01 -0400

    chat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import "./App.css";
+import Chat from "./components/Chat";
 import CreateOrJoin from "./components/CreateOrJoin";
-
 import Replicate from "./components/Replicate";
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-blue-400">conartAI</h1>
-      <Replicate />
-      <CreateOrJoin />
+      {/* <Replicate />
+      <CreateOrJoin /> */}
+      <Chat />
     </div>
   )
 }
