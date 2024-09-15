d06e6adf29902158c1e23b23d074ce3da6073d9d
ryushen-tan <78460980+ryushen-tan@users.noreply.github.com>
2024-09-14 23:14:21 -0400

    feat: mobile friendly

--- src/pages/CreateRoom.tsx
+++ src/pages/CreateRoom.tsx
@@ -39,22 +39,22 @@ const CreateRoom: React.FC = () => {
     <div className="w-screen h-screen bg-black overflow-hidden">
       <Navbar />
       <div className="flex items-center justify-center h-full">
-        <div className="w-[55vw] h-[25vw] bg-white bg-opacity-[10%] rounded-xl">
+        <div className='w-[55vw] h-[300px] md:h-[25vw] bg-white bg-opacity-[10%] rounded-xl'>
           <Region></Region>
           <div className="flex flex-col gap-8 justify-center w-full h-full items-center">
             <form onSubmit={handleCreateRoom}>
               <div className="relative">
                 <input
-                  className="bg-[#303030] px-2 w-[35vw] h-[4vw] rounded-2xl placeholder:absolute placeholder:text-[10px] placeholder:top-2 placeholder:left-3 placeholder:text-white"
-                  type="text"
+                    className="bg-[#303030] px-2 h-[30px] md:h-[4vw] w-[35vw] rounded-2xl placeholder:absolute placeholder:text-[10px] placeholder:top-2 placeholder:left-3 placeholder:text-white"
+                    type="text"
                   placeholder="Name"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
-              <button className="px-7 rounded-2xl py-3 w-[25vw] bg-white text-[#2b2b2b]">
-                Create Room
-              </button>
+              <div className="flex justify-center items-center">
+                <button className='px-7 md:text-[16px] mt-[2vw] text-[10px] rounded-2xl py-3 w-[30vw] bg-white text-[#2B2B2B]'>Create Room</button>
+              </div>
             </form>
           </div>
         </div>
