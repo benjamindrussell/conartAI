2a789ae81ce63f98825689af0aa4de34e9ec463e
benjamindrussell <ben@benjamindrussell.com>
2024-09-14 22:38:14 -0400

    game code

--- src/pages/Draw.tsx
+++ src/pages/Draw.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import Replicate from '../components/Replicate';
-import Logo from '../components/Logo';
 import Chat from '../components/Chat';
 import React from "react";
 import Replicate from "../components/Replicate";
@@ -11,24 +8,9 @@ import { api } from "../../convex/_generated/api";
 import { usePlayerStore } from "../store.ts";
 
 const Draw: React.FC = () => {
-  return (
-    <div className='flex flex-col w-screen h-screen bg-black'>
-      <div className='flex w-full gap-5 py-5'>
-        <Logo></Logo>
-        <div className='w-[9vw] ml-[7vw] shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)] bg-[#191919] h-[3vw] rounded-xl'></div>
-        {/* Timer Goes ^ */}
-        <button className='font-poppins font-regular shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)] bg-white w-[8vw] text-[14px] h-[3vw] rounded-xl text-black flex items-center justify-center'>Submit</button>
-      </div>
-      <div className='flex justify-evenly bg-black'>
-        <Replicate></Replicate>
-        <Chat></Chat>
-      </div>
-      <div className='flex flex-col'></div>
-    </div>
-  );
-}
-  let { gameCode } = useParams();
-  if (!gameCode) return null;
+  // let { gameCode } = useParams();
+  // if (!gameCode) return null;
+  const gameCode = "PXYJ";
 
   const room = useQuery(api.room.getRoom, { code: gameCode });
   const startGame = useMutation(api.room.startRoomGame);
@@ -90,9 +72,7 @@ const Draw: React.FC = () => {
           <Replicate />
         </div>
       </div>
-      <div className="">
-        <div></div>
-      </div>
+      <Chat></Chat>
       <div className="flex flex-col"></div>
     </div>
   );
